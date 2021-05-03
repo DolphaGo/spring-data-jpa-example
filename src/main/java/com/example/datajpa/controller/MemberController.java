931af6a49452c5cd91af8e2b75eba187f3011fe4
DolphaGo <adamdoha@naver.com>
2021-05-04 08:19:42 +0900

    Paging from Web

--- src/main/java/com/example/datajpa/controller/MemberController.java
+++ src/main/java/com/example/datajpa/controller/MemberController.java
@@ -2,10 +2,14 @@ package com.example.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.datajpa.dto.MemberDto;
 import com.example.datajpa.entity.Member;
 import com.example.datajpa.repository.MemberRepository;
 import com.example.datajpa.service.MemberService;
@@ -30,8 +34,24 @@ public class MemberController {
         return member.getUsername(); // 간단 간단할 때만 쓸 수 있다. 복잡하면 못쓴다.
     }
 
+    /**
+     * @implNote : 컨트롤러에서 파라미터가 바인딩 될 때, Pageable이 있으면 PageRequest를 생성해서 값을 채워줍니다. 스프링 자체가
+
+    근데 디폴트 size가 20개야. 이런걸 좀 바꾸고 싶어
+    1. 글로벌 설정(application.yml) -> spring.data.web.pageable.default-page-size : 10, max-page-size : 2000
+    2. 어노테이션 직접 설정(글로벌 설정보다 우선함) -> @PageDefault(size = 5, sort = "username" ...)
+     */
+    // 사용예 : http://localhost:8080/members?page=4&size=3&sort=id,desc
+    @GetMapping("/members")
+    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("DolphaGo"));
+        for (int i = 0; i < 1000; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
